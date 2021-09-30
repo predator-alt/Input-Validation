f691b4027dc1341245743b0a9cfabe6f81ca9947
Biplob <biplobsd11@gmail.com>
2021-10-01 00:27:35 +0600

    added shebang and add python __name__ main check

--- data_input.py
+++ data_input.py
@@ -1,13 +1,16 @@
-while True:
-	print("Enter your age:", end=" ")
-	age = input()
-	try:
-		age = int(age)
-	except ValueError:
-		print("Please use numeric digits.")
-		continue
-	if age<1:
-		print("Please enter a positive number.")
-		continue 
-	break
-print(f"Your age is {age}")
+#!/usr/bin/env python3
+
+if __name__=="__main__":
+	while True:
+		print("Enter your age:", end=" ")
+		age = input()
+		try:
+			age = int(age)
+		except ValueError:
+			print("Please use numeric digits.")
+			continue
+		if age<1:
+			print("Please enter a positive number.")
+			continue 
+		break
+	print(f"Your age is {age}")
